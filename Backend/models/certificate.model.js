bdcf06def07382f2af457eb2671a176ce62248ed
Gaurav Singh <76216972+Gauri1804@users.noreply.github.com>
2025-03-30 08:01:39 +0530

    Delete Backend directory

--- Backend/models/certificate.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const CertificateSchema = new mongoose.Schema({
-    student: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    course: { type: mongoose.Schema.Types.ObjectId, ref: "Course", required: true },
-    issuedAt: { type: Date, default: Date.now },
-});
-
-export const Certificate = mongoose.model("Certificate", CertificateSchema);
